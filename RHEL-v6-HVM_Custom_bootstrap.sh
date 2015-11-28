38f99d40ce1a5be68679c3eaa19ab5eecd659c53
Usui Takayuki <quantum777@gmail.com>
2015-11-29 04:28:33 +0900

    Create RHEL-v6-HVM_Custom_bootstrap.sh

---
+++ RHEL-v6-HVM_Custom_bootstrap.sh
@@ -0,0 +1,231 @@
+#!/bin/bash -v
+
+# Logger
+exec > >(tee /var/log/user-data.log || logger -t user-data -s 2> /dev/console) 2>&1
+
+# Instance MetaData
+region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//g')
+instanceId=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+privateIp=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+#-------------------------------------------------------------------------------
+# Default Package Update
+#-------------------------------------------------------------------------------
+
+# Red Hat Update Infrastructure Client Package Update
+yum clean all
+yum update -y rh-amazon-rhui-client
+
+# Enable Channnel (RHEL Server RPM) - [Default Enable]
+yum-config-manager --enable rhui-REGION-rhel-server-releases
+yum-config-manager --enable rhui-REGION-rhel-server-rh-common
+yum-config-manager --enable rhui-REGION-client-config-server-6
+
+# Enable Channnel (RHEL Server RPM) - [Default Disable]
+yum-config-manager --enable rhui-REGION-rhel-server-releases-optional
+yum-config-manager --enable rhui-REGION-rhel-server-supplementary
+#yum-config-manager --enable rhui-REGION-rhel-server-rhscl
+
+# Enable Channnel (RHEL Server Debug RPM)
+# yum-config-manager --enable rhui-REGION-rhel-server-debug-rh-common
+# yum-config-manager --enable rhui-REGION-rhel-server-debug-supplementary
+# yum-config-manager --enable rhui-REGION-rhel-server-debug-rhscl
+
+# Enable Channnel (RHEL Server Source RPM)
+# yum-config-manager --enable rhui-REGION-rhel-server-releases-source
+# yum-config-manager --enable rhui-REGION-rhel-server-source-rh-common
+# yum-config-manager --enable rhui-REGION-rhel-server-releases-optional-source
+# yum-config-manager --enable rhui-REGION-rhel-server-source-supplementary
+# yum-config-manager --enable rhui-REGION-rhel-server-source-rhscl
+
+# yum repository metadata Clean up
+yum clean all
+
+# Default Package Update
+yum update -y
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation
+#-------------------------------------------------------------------------------
+
+# Package Install RHEL System Administration Tools (from Red Hat Offical Repository)
+yum install -y git lzop yum-priorities yum-plugin-versionlock
+
+# Package Install EPEL(Extra Packages for Enterprise Linux) Repository Package
+yum localinstall -y http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/epel.repo
+yum clean all
+
+# Package Install RHEL System Administration Tools (from EPEL Recository)
+yum --enablerepo=epel install -y bash-completion jq
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS-CLI]
+#-------------------------------------------------------------------------------
+yum --enablerepo=epel install -y python-pip
+pip install --upgrade pip
+pip install awscli
+aws --version
+# aws ec2 describe-regions --region ${region}
+
+cat > /etc/profile.d/aws-cli.sh << __EOF__
+if [ -n "\$BASH_VERSION" ]; then
+   complete -C /usr/bin/aws_completer aws
+fi
+__EOF__
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS CloudFormation Helper Scripts]
+#-------------------------------------------------------------------------------
+
+# yum --enablerepo=epel localinstall -y https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.amzn1.noarch.rpm
+
+# yum --enablerepo=epel install -y python-pip
+# pip install --upgrade pip
+pip install pystache
+# pip install --upgrade argparse
+pip install --upgrade python-daemon
+# pip install --upgrade requests
+
+curl -O https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+tar -xvpf aws-cfn-bootstrap-latest.tar.gz
+
+cd aws-cfn-bootstrap-1.4/
+python setup.py build
+python setup.py install
+
+chmod 775 /usr/init/redhat/cfn-hup
+ln -s /usr/init/redhat/cfn-hup /etc/init.d/cfn-hup
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS-CloudWatchLogs-Agent]
+#-------------------------------------------------------------------------------
+# yum --enablerepo=epel install -y python-pip
+# pip install --upgrade pip
+curl -O https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
+
+cat > /tmp/awslogs.conf << __EOF__
+[general]
+state_file = /var/awslogs/state/agent-state
+
+[/var/log/messages]
+datetime_format = %b %d %H:%M:%S
+file = /var/log/messages
+buffer_duration = 5000
+log_stream_name = {instance_id}
+initial_position = start_of_file
+log_group_name = /var/log/messages
+encoding = utf-8
+
+[/var/log/secure]
+datetime_format = %b %d %H:%M:%S
+file = /var/log/secure
+buffer_duration = 5000
+log_stream_name = {instance_id}
+initial_position = start_of_file
+log_group_name = /var/log/secure
+encoding = utf-8
+__EOF__
+
+# python ./awslogs-agent-setup.py --region ${region} --configfile /tmp/awslogs.conf --non-interactive
+# service awslogs status
+# chkconfig --list awslogs
+# chkconfig awslogs on
+# chkconfig --list awslogs
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [Chef-Client(Chef-Solo)]
+#-------------------------------------------------------------------------------
+curl -L https://www.chef.io/chef/install.sh | bash -v
+mkdir -p /etc/chef/ohai/hints
+echo {} > /etc/chef/ohai/hints/ec2.json
+OHAI_PLUGINS="$(ohai | jq -r '.chef_packages.ohai.ohai_root + "/plugins"')"
+OHAI_PLUGINS_RACKERLABS="${OHAI_PLUGINS}/rackerlabs"
+mkdir -p ${OHAI_PLUGINS_RACKERLABS}
+curl -o ${OHAI_PLUGINS_RACKERLABS}/packages.rb https://raw.githubusercontent.com/rackerlabs/ohai-plugins/master/plugins/packages.rb
+curl -o ${OHAI_PLUGINS_RACKERLABS}/sshd.rb https://raw.githubusercontent.com/rackerlabs/ohai-plugins/master/plugins/sshd.rb
+curl -o ${OHAI_PLUGINS_RACKERLABS}/sysctl.rb https://raw.githubusercontent.com/rackerlabs/ohai-plugins/master/plugins/sysctl.rb
+ohai
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [Fluetnd(td-agent)]
+#-------------------------------------------------------------------------------
+
+# curl -L http://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh | bash -v
+rpm --import http://packages.treasuredata.com/GPG-KEY-td-agent
+
+cat > /etc/yum.repos.d/td.repo << __EOF__
+[treasuredata]
+name=TreasureData
+baseurl=http://packages.treasuredata.com/2/redhat/\$releasever/\$basearch
+gpgcheck=1
+gpgkey=https://packages.treasuredata.com/GPG-KEY-td-agent
+__EOF__
+
+yum install -y td-agent
+
+/opt/td-agent/embedded/bin/fluent-gem list --local
+#/opt/td-agent/embedded/bin/fluent-gem update ${gem-name}
+
+service td-agent start
+service td-agent status
+chkconfig --list td-agent
+chkconfig td-agent on
+chkconfig --list td-agent
+
+#-------------------------------------------------------------------------------
+# Custom Package Clean up
+#-------------------------------------------------------------------------------
+yum clean all
+
+#-------------------------------------------------------------------------------
+# System Setting
+#-------------------------------------------------------------------------------
+
+# Setting SystemClock
+cat > /etc/sysconfig/clock << __EOF__
+ZONE="Asia/Tokyo"
+UTC=false
+__EOF__
+
+# Setting TimeZone
+date
+/bin/cp -fp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+date
+ntpdate 0.rhel.pool.ntp.org
+date
+
+# Setting NTP Deamon
+sed -i 's/restrict -6/#restrict -6/g' /etc/ntp.conf
+service ntpd start
+chkconfig ntpd on
+
+# Setting Language
+cat > /etc/sysconfig/i18n << __EOF__
+LANG=ja_JP.UTF-8
+__EOF__
+
+# Firewall Service Disabled (iptables/ip6tables)
+service iptables stop
+chkconfig --list iptables
+chkconfig iptables off
+chkconfig --list iptables
+
+service ip6tables stop
+chkconfig --list ip6tables
+chkconfig ip6tables off
+chkconfig --list ip6tables
+
+# Disable IPv6 Kernel Module
+echo "options ipv6 disable=1" >> /etc/modprobe.d/ipv6.conf
+
+# Disable IPv6 Kernel Parameter
+sysctl -a
+echo "# Custom sysctl Parameter for ipv6 disable" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+sysctl -p
+sysctl -a | grep -ie "local_port" -ie "ipv6" | sort
+
+# Instance Reboot
+reboot
