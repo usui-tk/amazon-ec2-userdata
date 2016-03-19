3bd62bb922c274e37ecd45a3a344d89dc0b0fd26
Usui Takayuki <quantum777@gmail.com>
2016-03-20 03:04:17 +0900

    Update RHEL-v7-HVM_workaround-ExtendedDisk_bootstrap.sh

--- RHEL-v7-HVM_workaround-ExtendedDisk_bootstrap.sh
+++ RHEL-v7-HVM_workaround-ExtendedDisk_bootstrap.sh
@@ -3,19 +3,29 @@
 # Logger
 exec > >(tee /var/log/user-data.log || logger -t user-data -s 2> /dev/console) 2>&1
 
+# Instance MetaData
+region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//g')
+instanceId=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+instanceType=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
+privateIp=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+#-------------------------------------------------------------------------------
+# Default Package Update
+#-------------------------------------------------------------------------------
+
 # Red Hat Update Infrastructure Client Package Update
 yum clean all
 yum update -y rh-amazon-rhui-client
 
 # Enable Channnel (RHEL Server RPM) - [Default Enable]
-# yum-config-manager --enable rhui-REGION-rhel-server-releases
-# yum-config-manager --enable rhui-REGION-rhel-server-rh-common
-# yum-config-manager --enable rhui-REGION-client-config-server-7
+yum-config-manager --enable rhui-REGION-rhel-server-releases
+yum-config-manager --enable rhui-REGION-rhel-server-rh-common
+yum-config-manager --enable rhui-REGION-client-config-server-7
 
 # Enable Channnel (RHEL Server RPM) - [Default Disable]
 yum-config-manager --enable rhui-REGION-rhel-server-optional
 yum-config-manager --enable rhui-REGION-rhel-server-extras
-#yum-config-manager --enable rhui-REGION-rhel-server-rhscl
+# yum-config-manager --enable rhui-REGION-rhel-server-rhscl
 
 # Enable Channnel (RHEL Server Debug RPM)
 # yum-config-manager --enable rhui-REGION-rhel-server-releases-debug
@@ -35,54 +45,42 @@ yum-config-manager --enable rhui-REGION-rhel-server-extras
 yum clean all
 
 # Default Package Update
-yum update -y 
+yum update -y
 
-# Custom Package Install
-yum install -y bash-completion gdisk git yum-priorities yum-plugin-versionlock
+#-------------------------------------------------------------------------------
+# Custom Package Installation
+#-------------------------------------------------------------------------------
 
-# yum repository metadata Clean up
-yum clean all
+# Package Install RHEL System Administration Tools (from Red Hat Official Repository)
+yum install -y bash-completion dstat gdisk git lzop iotop mtr sos traceroute yum-priorities yum-plugin-versionlock
+yum install -y redhat-access-insights redhat-support-tool
+yum install -y setroubleshoot-server
 
-# Custom Package Install EPEL(Extra Packages for Enterprise Linux) repository Package
+# Package Install EPEL(Extra Packages for Enterprise Linux) Repository Package
 yum localinstall -y http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/epel.repo
 yum clean all
 
-# Custom Package Install (from EPEL)
-yum install -y jq
+# Package Install RHEL System Administration Tools (from EPEL Repository)
+yum --enablerepo=epel install -y jq
 
-# Custom Package Install Chef-Client(Chef-Solo)
-curl -L https://www.chef.io/chef/install.sh | bash -v
+#-------------------------------------------------------------------------------
+# Custom Package Clean up
+#-------------------------------------------------------------------------------
+yum clean all
 
-# Ohai EC2 Provider
-mkdir -p /etc/chef/ohai/hints
-echo {} > /etc/chef/ohai/hints/ec2.json
-
-# Custom Package Install Fluetnd(td-agent)
-# curl -L http://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh | bash -v
-rpm --import http://packages.treasuredata.com/GPG-KEY-td-agent
-
-cat > /etc/yum.repos.d/td.repo << __EOF__
-[treasuredata]
-name=TreasureData
-baseurl=http://packages.treasuredata.com/2/redhat/7/\$basearch
-gpgcheck=1
-gpgkey=http://packages.treasuredata.com/GPG-KEY-td-agent
-__EOF__
-
-yum install -y td-agent
-systemctl start td-agent
-systemctl status td-agent
-systemctl enable td-agent
-systemctl is-enabled td-agent
+#-------------------------------------------------------------------------------
+# System Setting
+#-------------------------------------------------------------------------------
 
 # Setting TimeZone
 # timedatectl status
-# timedatectl set-timezone Asia/Tokyo
+timedatectl set-timezone Asia/Tokyo
 # timedatectl status
 
 # Setting Language
 # localectl status
-# localectl set-locale LANG=ja_JP.utf8
+localectl set-locale LANG=ja_JP.utf8
 # localectl status
 
 # Setting NTP Deamon
@@ -94,7 +92,6 @@ sleep 3
 chronyc tracking
 chronyc sources -v
 chronyc sourcestats -v
-
 
 # Root Disk Partition Resize (GPT)
 # -- Use RHEL v7 HVM AMI (7.0_HVM_GA) --
@@ -135,12 +132,15 @@ echo "options ipv6 disable=1" >> /etc/modprobe.d/ipv6.conf
 
 # Disable IPv6 Kernel Parameter
 sysctl -a
-echo "# Custom sysctl Parameter for ipv6 disable" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+
+cat > /etc/sysctl.d/ipv6-disable.conf << __EOF__
+# Custom sysctl Parameter for ipv6 disable
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+__EOF__
+
 sysctl -p
 sysctl -a | grep -ie "local_port" -ie "ipv6" | sort
 
 # Instance Reboot
 reboot
-
