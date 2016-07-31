703e7fd1c5b73a9d889ac700487e4bb03198d849
Takayuki Usui <quantum777@gmail.com>
2016-07-31 23:34:52 +0900

    Update RHEL-v7-HVM_Custom_bootstrap.sh

--- RHEL-v7-HVM_Custom_bootstrap.sh
+++ RHEL-v7-HVM_Custom_bootstrap.sh
@@ -108,9 +108,11 @@ cat ~/.aws/config
 aws ec2 describe-regions --region ${Region}
 
 # Get EC2 Instance Information
+echo "# Get EC2 Instance Information"
 aws ec2 describe-instances --instance-ids ${InstanceId} --output json --region ${Region}
 
 # Get EC2 Instance attached EBS Volume Information
+echo "# Get EC2 Instance attached EBS Volume Information"
 aws ec2 describe-volumes --filters Name=attachment.instance-id,Values=${InstanceId} --output json --region ${Region}
 
 # Get EC2 Instance Attribute[Network Interface Performance Attribute]
