3e3770f8dda4843c971a19ae2fe7b96e17e7eb58
Takayuki Usui <quantum777@gmail.com>
2016-09-28 19:30:25 +0900

    Create AmazonLinux-2016.9.0-HVM_bootstrap.sh

---
+++ AmazonLinux-2016.9.0-HVM_bootstrap.sh
@@ -0,0 +1,391 @@
+#!/bin/bash -v
+
+# Logger
+exec > >(tee /var/log/user-data.log || logger -t user-data -s 2> /dev/console) 2>&1
+
+# Instance MetaData
+AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+Region=$(echo $AZ | sed -e 's/.$//g')
+InstanceId=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+InstanceType=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
+PrivateIp=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+AmiId=$(curl -s http://169.254.169.254/latest/meta-data/ami-id)
+
+#-------------------------------------------------------------------------------
+# Default Package Update
+#-------------------------------------------------------------------------------
+
+# yum repository metadata Clean up
+yum clean all
+
+# Check AmazonLinux-Preview Repository rpm-files
+yum --enablerepo=amzn-preview list | grep amzn-preview
+
+# Default Package Update
+yum update -y
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation
+#-------------------------------------------------------------------------------
+
+# Package Install Amazon Linux System Administration Tools (from Amazon Official Repository)
+yum install -y dstat git jq lzop iotop mtr nmap sos yum-plugin-versionlock wget
+yum install -y aws-cloudhsm-cli aws-kinesis-agent
+
+# Package Install Amazon Linux System Administration Tools (from EPEL Repository)
+yum --enablerepo=epel install -y bash-completion
+
+#-------------------------------------------------------------------------------
+# Getting IAM Role & STS Information
+#-------------------------------------------------------------------------------
+RoleArn=$(curl -s http://169.254.169.254/latest/meta-data/iam/info | jq -r '.InstanceProfileArn')
+RoleName=$(echo $RoleArn | cut -d '/' -f 2)
+
+StsCredential=$(curl -s "http://169.254.169.254/latest/meta-data/iam/security-credentials/$RoleName")
+StsAccessKeyId=$(echo $StsCredential | jq -r '.AccessKeyId')
+StsSecretAccessKey=$(echo $StsCredential | jq -r '.SecretAccessKey')
+StsToken=$(echo $StsCredential | jq -r '.Token')
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS-CLI]
+#-------------------------------------------------------------------------------
+aws --version
+
+# Setting AWS-CLI default Region & Output format
+aws configure << __EOF__ 
+
+
+${Region}
+json
+
+__EOF__
+
+sleep 3
+
+# Getting AWS-CLI default Region & Output format
+aws configure list
+cat ~/.aws/config
+
+# Get EC2 Region Information
+aws ec2 describe-regions --region ${Region}
+
+# Get AMI Information
+echo "# Get AMI Information"
+aws ec2 describe-images --image-ids ${AmiId} --output json --region ${Region}
+
+# Get EC2 Instance Information
+echo "# Get EC2 Instance Information"
+aws ec2 describe-instances --instance-ids ${InstanceId} --output json --region ${Region}
+
+# Get EC2 Instance attached EBS Volume Information
+echo "# Get EC2 Instance attached EBS Volume Information"
+aws ec2 describe-volumes --filters Name=attachment.instance-id,Values=${InstanceId} --output json --region ${Region}
+
+# Get EC2 Instance Attribute[Network Interface Performance Attribute]
+if [[ "$InstanceType" =~ ^(x1.*|m4.16xlarge)$ ]]; then
+	# Get EC2 Instance Attribute(Elastic Network Adapter Status)
+	echo "# Get EC2 Instance Attribute(Elastic Network Adapter Status)"
+	aws ec2 describe-instances --instance-id ${InstanceId} --query Reservations[].Instances[].EnaSupport --output json --region ${Region}
+	modinfo ena
+	ethtool -i eth0
+elif [[ "$InstanceType" =~ ^(c3.*|c4.*|d2.*|i2.*|m4.*|r3.*)$ ]]; then
+	# Get EC2 Instance Attribute(Single Root I/O Virtualization Status)
+	echo "# Get EC2 Instance Attribute(Single Root I/O Virtualization Status)"
+	aws ec2 describe-instance-attribute --instance-id ${InstanceId} --attribute sriovNetSupport --output json --region ${Region}
+	modinfo ixgbevf
+	ethtool -i eth0
+else
+	echo "Instance type of None [Network Interface Performance Attribute]"
+	ethtool -i eth0
+fi
+
+# Get EC2 Instance Attribute[Storage Interface Performance Attribute]
+if [[ "$InstanceType" =~ ^(c1.*|c3.*|c4.*|d2.*|g2.*|i2.*|m1.*|m2.*|m3.*|m4.*|r3.*)$ ]]; then
+	# Get EC2 Instance Attribute(EBS-optimized instance Status)
+	echo "# Get EC2 Instance Attribute(EBS-optimized instance Status)"
+	aws ec2 describe-instance-attribute --instance-id ${InstanceId} --attribute ebsOptimized --output json --region ${Region}
+else
+	echo "Instance type of None [Storage Interface Performance Attribute]"
+fi
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS-SHELL]
+#-------------------------------------------------------------------------------
+# pip install --upgrade pip
+# pip install --upgrade awscli
+pip install aws-shell
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [Amazon EC2 Simple Systems Manager (SSM) agent]
+#-------------------------------------------------------------------------------
+yum install -y amazon-ssm-agent
+
+status amazon-ssm-agent
+start amazon-ssm-agent
+status amazon-ssm-agent
+/sbin/restart amazon-ssm-agent
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS CodeDeploy Agent]
+#-------------------------------------------------------------------------------
+yum install -y ruby
+alternatives --display ruby
+
+# curl https://aws-codedeploy-ap-southeast-1.s3.amazonaws.com/latest/install -o /tmp/Install-AWS-CodeDeploy-Agent
+# curl https://aws-codedeploy-${Region}.s3.amazonaws.com/latest/install -o /tmp/Install-AWS-CodeDeploy-Agent
+
+curl https://aws-codedeploy-${Region}.s3.amazonaws.com/latest/install -o /tmp/Install-AWS-CodeDeploy-Agent
+
+chmod 744 /tmp/Install-AWS-CodeDeploy-Agent
+
+ruby /tmp/Install-AWS-CodeDeploy-Agent auto
+
+cat /opt/codedeploy-agent/.version
+
+service codedeploy-agent status
+chkconfig --list codedeploy-agent
+chkconfig codedeploy-agent on
+chkconfig --list codedeploy-agent
+service codedeploy-agent start
+service codedeploy-agent status
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [Amazon Inspector Agent]
+#-------------------------------------------------------------------------------
+curl https://d1wk0tztpsntt1.cloudfront.net/linux/latest/install -o /tmp/Install-Amazon-Inspector-Agent
+
+chmod 744 /tmp/Install-Amazon-Inspector-Agent
+bash /tmp/Install-Amazon-Inspector-Agent
+
+chkconfig --list awsagent
+chkconfig awsagent on
+chkconfig --list awsagent
+
+/opt/aws/awsagent/bin/awsagent status
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS CloudWatchLogs Agent] from PIP
+# ### [Workaround Installation Pattern] ###
+#-------------------------------------------------------------------------------
+curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -o /tmp/awslogs-agent-setup.py
+
+cat > /tmp/awslogs.conf << __EOF__
+[general]
+state_file = /var/awslogs/state/agent-state
+use_gzip_http_content_encoding = true
+
+[SYSTEM-sample-Linux-OS-var-log-messages]
+log_group_name = SYSTEM-sample-Linux-OS-var-log-messages
+log_stream_name = {instance_id}
+datetime_format = %b %d %H:%M:%S
+time_zone = LOCAL
+file = /var/log/messages
+initial_position = start_of_file
+encoding = utf-8
+buffer_duration = 5000
+
+[SYSTEM-sample-Linux-OS-var-log-secure]
+log_group_name = SYSTEM-sample-Linux-OS-var-log-secure
+log_stream_name = {instance_id}
+datetime_format = %b %d %H:%M:%S
+time_zone = LOCAL
+file = /var/log/secure
+initial_position = start_of_file
+encoding = utf-8
+buffer_duration = 5000
+
+[SYSTEM-sample-Linux-SSM-Agent-Logs]
+log_group_name = SYSTEM-sample-Linux-SSM-Agent-Logs
+log_stream_name = {instance_id}
+datetime_format = %Y-%m-%d %H:%M:%S
+time_zone = LOCAL
+file = /var/log/amazon/ssm/amazon-ssm-agent.log
+initial_position = start_of_file
+encoding = ascii
+buffer_duration = 5000
+
+[SYSTEM-sample-Linux-CodeDeploy-Agent-Logs]
+log_group_name = SYSTEM-sample-Linux-CodeDeploy-Agent-Logs
+log_stream_name = {instance_id}
+datetime_format = %Y-%m-%d %H:%M:%S
+time_zone = LOCAL
+file = /var/log/aws/codedeploy-agent/codedeploy-agent.log
+initial_position = start_of_file
+encoding = ascii
+buffer_duration = 5000
+
+__EOF__
+
+python /tmp/awslogs-agent-setup.py --region ${Region} --configfile /tmp/awslogs.conf --non-interactive
+
+service awslogs status
+chkconfig --list awslogs
+chkconfig awslogs on
+chkconfig --list awslogs
+service awslogs restart
+service awslogs status
+
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS CloudWatchLogs Agent] from RPM
+# ### [General Installation Pattern] ###
+#-------------------------------------------------------------------------------
+
+# yum install -y awslogs aws-cli-plugin-cloudwatch-logs
+
+# sed -i "s/region = us-east-1/region = ${Region}/g" /etc/awslogs/awscli.conf
+
+# cat > /etc/awslogs/awslogs.conf << __EOF__
+# [general]
+# state_file = /var/awslogs/state/agent-state
+# use_gzip_http_content_encoding = true
+# 
+# [SYSTEM-sample-Linux-OS-var-log-messages]
+# log_group_name = SYSTEM-sample-Linux-OS-var-log-messages
+# log_stream_name = {instance_id}
+# datetime_format = %b %d %H:%M:%S
+# time_zone = LOCAL
+# file = /var/log/messages
+# initial_position = start_of_file
+# encoding = utf-8
+# buffer_duration = 5000
+# 
+# [SYSTEM-sample-Linux-OS-var-log-secure]
+# log_group_name = SYSTEM-sample-Linux-OS-var-log-secure
+# log_stream_name = {instance_id}
+# datetime_format = %b %d %H:%M:%S
+# time_zone = LOCAL
+# file = /var/log/secure
+# initial_position = start_of_file
+# encoding = utf-8
+# buffer_duration = 5000
+# 
+# [SYSTEM-sample-Linux-SSM-Agent-Logs]
+# log_group_name = SYSTEM-sample-Linux-SSM-Agent-Logs
+# log_stream_name = {instance_id}
+# datetime_format = %Y-%m-%d %H:%M:%S
+# time_zone = LOCAL
+# file = /var/log/amazon/ssm/amazon-ssm-agent.log
+# initial_position = start_of_file
+# encoding = ascii
+# buffer_duration = 5000
+# 
+# [SYSTEM-sample-Linux-CodeDeploy-Agent-Logs]
+# log_group_name = SYSTEM-sample-Linux-CodeDeploy-Agent-Logs
+# log_stream_name = {instance_id}
+# datetime_format = %Y-%m-%d %H:%M:%S
+# time_zone = LOCAL
+# file = /var/log/aws/codedeploy-agent/codedeploy-agent.log
+# initial_position = start_of_file
+# encoding = ascii
+# buffer_duration = 5000
+# 
+# __EOF__
+
+# service awslogs start
+# service awslogs status
+# chkconfig --list awslogs
+# chkconfig awslogs on
+# chkconfig --list awslogs
+
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [Ansible]
+#-------------------------------------------------------------------------------
+yum --enablerepo=epel install -y ansible
+
+ansible --version
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [Fluetnd(td-agent)]
+#-------------------------------------------------------------------------------
+# curl -L http://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh | bash
+rpm --import http://packages.treasuredata.com/GPG-KEY-td-agent
+
+cat > /etc/yum.repos.d/td.repo << __EOF__
+[treasuredata]
+name=TreasureData
+baseurl=http://packages.treasuredata.com/2/redhat/\$releasever/\$basearch
+gpgcheck=1
+gpgkey=https://packages.treasuredata.com/GPG-KEY-td-agent
+__EOF__
+
+yum install -y td-agent
+
+td-agent-gem list --local
+td-agent-gem install fluent-plugin-aws-elasticsearch-service
+td-agent-gem install fluent-plugin-cloudwatch-logs
+td-agent-gem install fluent-plugin-kinesis
+td-agent-gem install fluent-plugin-kinesis-firehose
+td-agent-gem update fluent-plugin-s3
+td-agent-gem list --local
+
+service td-agent start
+service td-agent status
+chkconfig --list td-agent
+chkconfig td-agent on
+chkconfig --list td-agent
+
+#-------------------------------------------------------------------------------
+# Custom Package Clean up
+#-------------------------------------------------------------------------------
+yum clean all
+
+#-------------------------------------------------------------------------------
+# System Setting
+#-------------------------------------------------------------------------------
+
+# Setting SystemClock
+cat > /etc/sysconfig/clock << __EOF__
+ZONE="Asia/Tokyo"
+UTC=false
+__EOF__
+
+# Setting TimeZone
+date
+/bin/cp -fp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+date
+ntpdate 0.amazon.pool.ntp.org
+date
+
+# Setting NTP Deamon
+sed -i 's/restrict -6/#restrict -6/g' /etc/ntp.conf
+service ntpd restart
+chkconfig ntpd on
+
+# Setting Language
+cat > /etc/sysconfig/i18n << __EOF__
+LANG=ja_JP.UTF-8
+__EOF__
+
+# Ephemeral-Disk Auto Mount Disabled (cloud-init)
+sed -i '/ephemeral0/d' /etc/cloud/cloud.cfg
+
+# Firewall Service Disabled (iptables/ip6tables)
+service iptables stop
+chkconfig --list iptables
+chkconfig iptables off
+chkconfig --list iptables
+
+service ip6tables stop
+chkconfig --list ip6tables
+chkconfig ip6tables off
+chkconfig --list ip6tables
+
+# Disable IPv6 Kernel Module
+echo "options ipv6 disable=1" >> /etc/modprobe.d/ipv6.conf
+
+# Disable IPv6 Kernel Parameter
+sysctl -a
+
+cat > /etc/sysctl.d/99-ipv6-disable.conf << __EOF__
+# Custom sysctl Parameter for ipv6 disable
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+__EOF__
+
+sysctl -p
+sysctl -a | grep -ie "local_port" -ie "ipv6" | sort
+
+# Instance Reboot
+reboot
