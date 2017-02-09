e47b532c85649ef653ddb246cff605a3860ed159
Takayuki Usui <quantum777@gmail.com>
2017-02-10 01:14:36 +0900

    Bugfix

--- VPC-IPv4/2nd-Select_Linux.sh
+++ VPC-IPv4/2nd-Select_Linux.sh
@@ -73,10 +73,10 @@ function get_os_info () {
 
     if [ "${DIST_TYPE}" = "Amazon" ] || [ "${DIST_TYPE}" = "amzn" ]; then
           BootstrapScript=${BootstrapAmazonLinux}
-    elif [ "${DIST_TYPE}" = "RHEL" ] || [ "${DIST_TYPE}" = "RedHat" ]; then
-        if [ "${REV}" = "7" ]; then
+    elif [ "${DIST_TYPE}" = "RHEL" ] || [ "${DIST_TYPE}" = "rhel" ]; then
+        if [ "${REV}" = "7.*" ]; then
            BootstrapScript=${BootstrapRHELv7}
-        elif [ "${REV}" = "6" ]; then
+        elif [ "${REV}" = "6.*" ]; then
            BootstrapScript=${BootstrapRHELv6}
         else
            BootstrapScript=""
