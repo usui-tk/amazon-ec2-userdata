e84f65d2a319093f83ff44e35549b383d0cbec31
Takayuki Usui <quantum777@gmail.com>
2017-02-21 02:36:28 +0900

    Ubuntu 16.04LTS

---
+++ 3rd-Bootstrap_Ubuntu-16.04-LTS-HVM.sh
@@ -0,0 +1,377 @@
+#!/bin/bash -v
+
+# Logger
+exec > >(tee /var/log/user-data_3rd-bootstrap.log || logger -t user-data -s 2> /dev/console) 2>&1
+
+#-------------------------------------------------------------------------------
+# Set UserData Parameter
+#-------------------------------------------------------------------------------
+
+if [ -f /tmp/userdata-parameter ]; then
+    source /tmp/userdata-parameter
+	# echo
+	echo $Language
+	echo $Timezone
+	echo $VpcNetwork
+fi
+
+if [[ -z "${Language}" || -z "${Timezone}" || -z "${VpcNetwork}" ]]; then
+    # Default Language
+	Language="ja_JP.UTF-8"
+    # Default Timezone
+	Timezone="Asia/Tokyo"
+	# Default VPC Network
+	VpcNetwork="IPv4"
+	# echo
+	echo $Language
+	echo $Timezone
+	echo $VpcNetwork
+fi
+
+#-------------------------------------------------------------------------------
+# Default Package Update
+#-------------------------------------------------------------------------------
+
+# Command Non-Interactive Mode
+DEBIAN_FRONTEND=noninteractive
+
+# yum repository metadata Clean up
+apt-get -y clean
+
+# Default Package Update
+apt-get -y update
+apt-get -y upgrade
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation
+#-------------------------------------------------------------------------------
+
+# Package Install Ubuntu System Administration Tools (from Ubuntu Official Repository)
+apt-get install -y bash-completion chrony curl dstat gdisk git jq lsof lzop iotop mtr nmap sysstat traceroute wget
+
+#-------------------------------------------------------------------------------
+# Set AWS Instance MetaData
+#-------------------------------------------------------------------------------
+
+# Instance MetaData
+AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+Region=$(echo $AZ | sed -e 's/.$//g')
+InstanceId=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+InstanceType=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
+PrivateIp=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+AmiId=$(curl -s http://169.254.169.254/latest/meta-data/ami-id)
+
+# IAM Role & STS Information
+RoleArn=$(curl -s http://169.254.169.254/latest/meta-data/iam/info | jq -r '.InstanceProfileArn')
+RoleName=$(echo $RoleArn | cut -d '/' -f 2)
+
+StsCredential=$(curl -s "http://169.254.169.254/latest/meta-data/iam/security-credentials/$RoleName")
+StsAccessKeyId=$(echo $StsCredential | jq -r '.AccessKeyId')
+StsSecretAccessKey=$(echo $StsCredential | jq -r '.SecretAccessKey')
+StsToken=$(echo $StsCredential | jq -r '.Token')
+
+# AWS Account ID
+AwsAccountId=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.accountId')
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS-CLI]
+#-------------------------------------------------------------------------------
+# apt-get install -y python3-pip
+apt-get install -y awscli
+
+cat > /etc/profile.d/aws-cli.sh << __EOF__
+if [ -n "\$BASH_VERSION" ]; then
+   complete -C /usr/bin/aws_completer aws
+fi
+__EOF__
+
+aws --version
+
+# Setting AWS-CLI default Region & Output format
+aws configure << __EOF__ 
+
+
+${Region}
+json
+
+__EOF__
+
+sleep 3
+
+# Getting AWS-CLI default Region & Output format
+aws configure list
+cat ~/.aws/config
+
+# Get AWS Region Information
+if [ -n "$RoleName" ]; then
+	echo "# Get AWS Region Infomation"
+	aws ec2 describe-regions --region ${Region}
+fi
+
+# Get AMI Information
+if [ -n "$RoleName" ]; then
+	echo "# Get AMI Information"
+	aws ec2 describe-images --image-ids ${AmiId} --output json --region ${Region}
+fi
+
+# Get EC2 Instance Information
+if [ -n "$RoleName" ]; then
+	echo "# Get EC2 Instance Information"
+	aws ec2 describe-instances --instance-ids ${InstanceId} --output json --region ${Region}
+fi
+
+# Get EC2 Instance attached EBS Volume Information
+if [ -n "$RoleName" ]; then
+	echo "# Get EC2 Instance attached EBS Volume Information"
+	aws ec2 describe-volumes --filters Name=attachment.instance-id,Values=${InstanceId} --output json --region ${Region}
+fi
+
+# Get EC2 Instance Attribute[Network Interface Performance Attribute]
+if [ -n "$RoleName" ]; then
+	if [[ "$InstanceType" =~ ^(x1.*|p2.*|r4.*|m4.16xlarge)$ ]]; then
+		# Get EC2 Instance Attribute(Elastic Network Adapter Status)
+		echo "# Get EC2 Instance Attribute(Elastic Network Adapter Status)"
+		aws ec2 describe-instances --instance-id ${InstanceId} --query Reservations[].Instances[].EnaSupport --output json --region ${Region}
+		echo "# Get Linux Kernel Module(modinfo ena)"
+		modinfo ena
+		echo "# Get Linux Network Interface Driver(ethtool -i eth0)"
+		ethtool -i ens3
+	elif [[ "$InstanceType" =~ ^(c3.*|c4.*|d2.*|i2.*|m4.*|r3.*)$ ]]; then
+		# Get EC2 Instance Attribute(Single Root I/O Virtualization Status)
+		echo "# Get EC2 Instance Attribute(Single Root I/O Virtualization Status)"
+		aws ec2 describe-instance-attribute --instance-id ${InstanceId} --attribute sriovNetSupport --output json --region ${Region}
+		echo "# Get Linux Kernel Module(modinfo ixgbevf)"
+		modinfo ixgbevf
+		echo "# Get Linux Network Interface Driver(ethtool -i eth0)"
+		ethtool -i ens3
+	else
+		echo "# Get Linux Network Interface Driver(ethtool -i eth0)"
+		ethtool -i ens3
+	fi
+else
+	echo "# Get Linux Network Interface Driver(ethtool -i eth0)"
+	ethtool -i ens3
+fi
+
+# Get EC2 Instance Attribute[Storage Interface Performance Attribute]
+if [ -n "$RoleName" ]; then
+	if [[ "$InstanceType" =~ ^(c1.*|c3.*|c4.*|d2.*|g2.*|i2.*|m1.*|m2.*|m3.*|m4.*|p2.*|r3.*|r4.*|x1.*)$ ]]; then
+		# Get EC2 Instance Attribute(EBS-optimized instance Status)
+		echo "# Get EC2 Instance Attribute(EBS-optimized instance Status)"
+		aws ec2 describe-instance-attribute --instance-id ${InstanceId} --attribute ebsOptimized --output json --region ${Region}
+		echo "# Get Linux Block Device Read-Ahead Value(blockdev --report)"
+		blockdev --report
+	else
+		echo "# Get Linux Block Device Read-Ahead Value(blockdev --report)"
+		blockdev --report
+	fi
+else
+	echo "# Get Linux Block Device Read-Ahead Value(blockdev --report)"
+	blockdev --report
+fi
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [AWS CloudFormation Helper Scripts]
+#-------------------------------------------------------------------------------
+apt-get install -y python-setuptools
+easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+
+ln -s /usr/local/lib/python2.7/dist-packages/aws_cfn_bootstrap-1.4-py2.7.egg/init/ubuntu/cfn-hup /etc/init.d/cfn-hup
+chmod +x /etc/init.d/cfn-hup
+update-rc.d cfn-hup defaults
+service cfn-hup start
+
+#-------------------------------------------------------------------------------
+# Custom Package Installation [Amazon EC2 Simple Systems Manager (SSM) agent]
+#-------------------------------------------------------------------------------
+cd /tmp			
+curl https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb -o amazon-ssm-agent.deb
+dpkg -i amazon-ssm-agent.deb
+
+# Workaround (https://github.com/aws/amazon-ssm-agent/pull/33)
+cat > /lib/systemd/system/amazon-ssm-agent.service << __EOF__
+[Unit]
+Description=amazon-ssm-agent
+After=network-online.target
+
+[Service]
+Type=simple
+WorkingDirectory=/usr/bin/
+ExecStart=/usr/bin/amazon-ssm-agent
+KillMode=process
+Restart=on-failure
+RestartSec=15min
+
+[Install]
+WantedBy=network-online.target
+__EOF__
+
+systemctl daemon-reload
+
+systemctl status amazon-ssm-agent
+systemctl enable amazon-ssm-agent
+systemctl is-enabled amazon-ssm-agent
+
+systemctl restart amazon-ssm-agent
+systemctl status amazon-ssm-agent
+
+#-------------------------------------------------------------------------------
+# Custom Package Clean up
+#-------------------------------------------------------------------------------
+apt-get -y clean
+
+#-------------------------------------------------------------------------------
+# System Setting
+#-------------------------------------------------------------------------------
+
+# CPU Information [cat /proc/cpuinfo]
+cat /proc/cpuinfo
+
+# CPU Information [lscpu]
+lscpu
+
+# Memory Information [cat /proc/meminfo]
+cat /proc/meminfo
+
+# Memory Information [free]
+free
+
+# Disk Information(Partition) [parted -l]
+parted -l
+
+# Disk Information(MountPoint) [lsblk]
+lsblk
+
+# Disk Information(File System) [df -h]
+df -h
+
+# Network Information(Network Interface) [ip addr show]
+ip addr show
+
+# Network Information(Routing Table) [ip route show]
+ip route show
+
+# Network Information(Firewall Service) [Uncomplicated firewall]
+if [ $(command -v ufw) ]; then
+    # Network Information(Firewall Service) [systemctl status ufw]
+    systemctl status ufw
+    # Network Information(Firewall Service Status) [ufw status]
+    ufw status verbose
+    # Network Information(Firewall Service Disabled) [ufw disable]
+    ufw disable
+    # Network Information(Firewall Service Disabled) [systemctl disable & stop ufw]
+    systemctl disable ufw
+	systemctl status ufw
+	systemctl stop ufw
+	systemctl status ufw
+fi
+
+#-------------------------------------------------------------------------------
+# System Setting
+#-------------------------------------------------------------------------------
+
+# NTP Service Enabled(chrony)
+systemctl restart chrony
+systemctl enable chrony
+systemctl is-enabled chrony
+sleep 3
+chronyc tracking
+chronyc sources -v
+chronyc sourcestats -v
+
+# Setting SystemClock and Timezone
+if [ "${Timezone}" = "Asia/Tokyo" ]; then
+	echo "# Setting SystemClock and Timezone -> $Timezone"
+	date
+	# timedatectl status
+	timedatectl set-timezone Asia/Tokyo
+	date
+	# timedatectl status
+elif [ "${Timezone}" = "UTC" ]; then
+	echo "# Setting SystemClock and Timezone -> $Timezone"
+	date
+	# timedatectl status
+	timedatectl set-timezone UTC
+	date
+	# timedatectl status
+else
+	echo "# Default SystemClock and Timezone"
+	# timedatectl status
+	date
+fi
+
+# Time synchronization with NTP server
+date
+chronyc tracking
+chronyc sources -v
+chronyc sourcestats -v
+date
+
+# Setting System Language
+if [ "${Language}" = "ja_JP.UTF-8" ]; then
+	# Custom Package Installation [language-pack-ja]
+	apt-get install -y language-pack-ja fonts-ipafont
+	echo "# Setting System Language -> $Language"
+	locale
+	# localectl status
+	localectl set-locale LANG=ja_JP.utf8
+	locale
+	# localectl status
+	cat /etc/locale.gen | grep -v "#"
+elif [ "${Language}" = "en_US.UTF-8" ]; then
+	echo "# Setting System Language -> $Language"
+	locale
+	# localectl status
+	localectl set-locale LANG=en_US.utf8
+	locale
+	# localectl status
+	cat /etc/locale.gen | grep -v "#"
+else
+	echo "# Default Language"
+	locale
+	cat /etc/locale.gen | grep -v "#"
+fi
+
+# Setting IP Protocol Stack (IPv4 Only) or (IPv4/IPv6 Dual stack)
+if [ "${VpcNetwork}" = "IPv4" ]; then
+	echo "# Setting IP Protocol Stack -> $VpcNetwork"
+	# Disable IPv6 Kernel Module
+	echo "options ipv6 disable=1" >> /etc/modprobe.d/ipv6.conf
+	# Disable IPv6 Kernel Parameter
+	sysctl -a
+
+	DisableIPv6Conf="/etc/sysctl.d/99-ipv6-disable.conf"
+
+	cat /dev/null > $DisableIPv6Conf
+	echo '# Custom sysctl Parameter for ipv6 disable' >> $DisableIPv6Conf
+	echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> $DisableIPv6Conf
+	echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> $DisableIPv6Conf
+
+	sysctl --system
+	sysctl -p
+
+	sysctl -a | grep -ie "local_port" -ie "ipv6" | sort
+elif [ "${Timezone}" = "IPv6" ]; then
+	echo "# Show IP Protocol Stack -> $VpcNetwork"
+	echo "# Show IPv6 Network Interface Address"
+	ifconfig
+	echo "# Show IPv6 Kernel Module"
+	lsmod | grep ipv6
+	echo "# Show Network Listen Address and report"
+	netstat -an -A inet6
+	echo "# Show Network Routing Table"
+	netstat -r -A inet6
+else
+	echo "# Default IP Protocol Stack"
+	echo "# Show IPv6 Network Interface Address"
+	ifconfig
+	echo "# Show IPv6 Kernel Module"
+	lsmod | grep ipv6
+	echo "# Show Network Listen Address and report"
+	netstat -an -A inet6
+	echo "# Show Network Routing Table"
+	netstat -r -A inet6
+fi
+
+# Instance Reboot
+reboot
