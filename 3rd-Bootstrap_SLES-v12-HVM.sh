288ab153bc16df1995172768a336f42523ec95cd
Takayuki Usui <quantum777@gmail.com>
2017-07-22 21:27:59 +0900

    Update for AWS EC2 G3 Instance

--- 3rd-Bootstrap_SLES-v12-HVM.sh
+++ 3rd-Bootstrap_SLES-v12-HVM.sh
@@ -146,7 +146,7 @@ fi
 
 # Get EC2 Instance Attribute[Network Interface Performance Attribute]
 if [ -n "$RoleName" ]; then
-	if [[ "$InstanceType" =~ ^(i3.*|m4.16xlarge|p2.*|r4.*|x1.*)$ ]]; then
+	if [[ "$InstanceType" =~ ^(g3.*|i3.*|m4.16xlarge|p2.*|r4.*|x1.*)$ ]]; then
 		# Get EC2 Instance Attribute(Elastic Network Adapter Status)
 		echo "# Get EC2 Instance Attribute(Elastic Network Adapter Status)"
 		aws ec2 describe-instances --instance-id ${InstanceId} --query Reservations[].Instances[].EnaSupport --output json --region ${Region}
@@ -165,7 +165,7 @@ fi
 
 # Get EC2 Instance Attribute[Storage Interface Performance Attribute]
 if [ -n "$RoleName" ]; then
-	if [[ "$InstanceType" =~ ^(c1.*|c3.*|c4.*|d2.*|g2.*|i2.*|i3.*|m1.*|m2.*|m3.*|m4.*|p2.*|r3.*|r4.*|x1.*)$ ]]; then
+	if [[ "$InstanceType" =~ ^(c1.*|c3.*|c4.*|d2.*|g2.*|g3.*|i2.*|i3.*|m1.*|m2.*|m3.*|m4.*|p2.*|r3.*|r4.*|x1.*)$ ]]; then
 		# Get EC2 Instance Attribute(EBS-optimized instance Status)
 		echo "# Get EC2 Instance Attribute(EBS-optimized instance Status)"
 		aws ec2 describe-instance-attribute --instance-id ${InstanceId} --attribute ebsOptimized --output json --region ${Region}
