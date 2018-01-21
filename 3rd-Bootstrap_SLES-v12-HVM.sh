02ea1f8b30cf564912bfc27d83c88cc1ef9031cc
Takayuki Usui <quantum777@gmail.com>
2018-01-21 21:10:44 +0900

    Update

--- 3rd-Bootstrap_SLES-v12-HVM.sh
+++ 3rd-Bootstrap_SLES-v12-HVM.sh
@@ -85,12 +85,13 @@ zypper --non-interactive update
 #-------------------------------------------------------------------------------
 
 # Package Install SLES System Administration Tools (from SUSE Linux Enterprise Server Software repository)
-zypper --non-interactive install arptables bash-completion dstat ebtables git-core hdparm hostinfo iotop lzop nmap nvme-cli sdparm supportutils sysstat systemd-bash-completion time traceroute unzip zypper-log
+zypper --non-interactive install arptables bash-completion dstat ebtables git-core hdparm hostinfo iotop lzop nmap nvme-cli sdparm supportutils supportutils-plugin-suse-public-cloud sysstat systemd-bash-completion time traceroute unzip zypper-log
+zypper --non-interactive install patterns-sles-apparmor
 
 # Package Install SLES System AWS Tools (from SUSE Linux Enterprise Server Software repository)
 #  zypper --non-interactive install patterns-public-cloud-Amazon-Web-Services
-#  zypper --non-interactive install patterns-public-cloud-Amazon-Web-Services-Instance-Init
-#  zypper --non-interactive install patterns-public-cloud-Amazon-Web-Services-Instance-Tools
+zypper --non-interactive install patterns-public-cloud-Amazon-Web-Services-Instance-Init
+zypper --non-interactive install patterns-public-cloud-Amazon-Web-Services-Instance-Tools
 zypper --non-interactive install patterns-public-cloud-Amazon-Web-Services-Tools
 
 
