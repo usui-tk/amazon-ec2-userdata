75c3bee2f8c5a25cfd65e8c7de4177e9612a26d9
Takayuki Usui <quantum777@gmail.com>
2019-09-11 05:36:28 +0900

    Update 3rd-Bootstrap_Debian-10-HVM.sh

--- 3rd-Bootstrap_Debian-10-HVM.sh
+++ 3rd-Bootstrap_Debian-10-HVM.sh
@@ -177,7 +177,8 @@ fi
 # Get the latest AMI information of the OS type of this EC2 instance from Public AMI
 if [ -n "$RoleName" ]; then
 	echo "# Get Newest AMI Information from Public AMI"
-	NewestAmiInfo=$(aws ec2 describe-images --owner "903794441882" --filter "Name=name,Values=debian-buster-hvm-x86_64-gp2-*" "Name=virtualization-type,Values=hvm" "Name=architecture,Values=x86_64" --query 'sort_by(Images[].{YMD:CreationDate,Name:Name,ImageId:ImageId},&YMD)|reverse(@)|[0]' --output json --region ${Region})
+	NewestAmiInfo=$(aws ec2 describe-images --owner "903794441882" --filter "Name=name,Values=debian-10-amd64-daily-*" "Name=virtualization-type,Values=hvm" "Name=architecture,Values=x86_64" --query 'sort_by(Images[].{YMD:CreationDate,Name:Name,ImageId:ImageId},&YMD)|reverse(@)|[0]' --output json --region ${Region})
+	# NewestAmiInfo=$(aws ec2 describe-images --owner "903794441882" --filter "Name=name,Values=debian-buster-hvm-x86_64-gp2-*" "Name=virtualization-type,Values=hvm" "Name=architecture,Values=x86_64" --query 'sort_by(Images[].{YMD:CreationDate,Name:Name,ImageId:ImageId},&YMD)|reverse(@)|[0]' --output json --region ${Region})
 	NewestAmiId=$(echo $NewestAmiInfo| jq -r '.ImageId')
 	aws ec2 describe-images --image-ids ${NewestAmiId} --output json --region ${Region}
 fi
