f729dc4526e96432c695e53bbac9e3da23da0c35
Takayuki Usui <quantum777@gmail.com>
2019-09-12 20:07:36 +0900

    Update 3rd-Bootstrap_Ubuntu-16.04-LTS-HVM.sh

--- 3rd-Bootstrap_Ubuntu-16.04-LTS-HVM.sh
+++ 3rd-Bootstrap_Ubuntu-16.04-LTS-HVM.sh
@@ -393,6 +393,9 @@ snap services amazon-ssm-agent
 # https://docs.aws.amazon.com/inspector/latest/userguide/inspector_installing-uninstalling-agents.html
 #-------------------------------------------------------------------------------
 
+# Introduction of additional packages for ubuntu
+apt install -y -q libcurl3
+
 # Variable initialization
 InspectorInstallStatus="0"
 
@@ -423,6 +426,9 @@ if [ $InspectorInstallStatus -eq 0 ]; then
 else
 	echo "Failed to execute Amazon Inspector Agent installer script"
 fi
+
+# Cleanup
+apt clean -y -q
 
 #-------------------------------------------------------------------------------
 # Custom Package Install [Amazon CloudWatch Agent]
