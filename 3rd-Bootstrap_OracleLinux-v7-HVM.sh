3e37f9a7c68b5ac14262eb28caa47d558671336d
Takayuki Usui <quantum777@gmail.com>
2019-10-27 19:22:14 +0900

    Update

--- 3rd-Bootstrap_OracleLinux-v7-HVM.sh
+++ 3rd-Bootstrap_OracleLinux-v7-HVM.sh
@@ -1008,7 +1008,7 @@ df -h
 # Configure cloud-init/disk_setup module
 cat /etc/cloud/cloud.cfg
 
-if [ $(grep -q growpart /etc/cloud/cloud.cfg) ]; then
+if [ $(grep -q disk_setup /etc/cloud/cloud.cfg) ]; then
 	cat /etc/cloud/cloud.cfg
 else
 	sed -i 's/ - migrator/ - disk_setup\n - migrator/' /etc/cloud/cloud.cfg
