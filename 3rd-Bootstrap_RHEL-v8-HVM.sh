0d1f07c635090a680e9af05ffd37e3058d5ff595
Takayuki Usui <quantum777@gmail.com>
2019-11-03 23:53:25 +0900

    Update 3rd-Bootstrap_RHEL-v8-HVM.sh

--- 3rd-Bootstrap_RHEL-v8-HVM.sh
+++ 3rd-Bootstrap_RHEL-v8-HVM.sh
@@ -164,7 +164,7 @@ dnf update -y
 #-------------------------------------------------------------------------------
 
 # Package Install RHEL System Administration Tools (from Red Hat Official Repository)
-dnf install -y acpid arptables bash-completion bc bcc bcc-tools bind-utils blktrace bpftool crash-trace-command crypto-policies curl dstat ebtables ethtool expect fio gdisk git gnutls-utils hdparm intltool iotop iperf3 iptraf-ng jq kexec-tools libicu lsof lvm2 lzop man-pages mcelog mdadm mlocate mtr nc ncompress net-snmp-utils nftables nmap numactl nvme-cli nvmetcli patchutils pmempool psacct psmisc rsync smartmontools sos strace symlinks sysfsutils sysstat tcpdump time tlog traceroute tree unzip util-linux util-linux-user vdo vim-enhanced wget xfsdump xfsprogs zip zsh
+dnf install -y abrt abrt-cli acpid arptables bash-completion bc bcc bcc-tools bind-utils blktrace bpftool crash-trace-command crypto-policies curl dstat ebtables ethtool expect fio gdisk git gnutls-utils hdparm intltool iotop iperf3 iptraf-ng jq kexec-tools libicu lsof lvm2 lzop man-pages mc mcelog mdadm mlocate mtr nc ncompress net-snmp-utils nftables nmap numactl nvme-cli nvmetcli parted patchutils pmempool psacct psmisc python3-dnf-plugin-versionlock rsync smartmontools sos strace symlinks sysfsutils sysstat tcpdump time tlog tmpwatch traceroute tree tzdata unzip usermode util-linux util-linux-user vdo vim-enhanced wget xfsdump xfsprogs zip zsh
 dnf install -y cifs-utils nfs-utils nfs4-acl-tools
 dnf install -y iscsi-initiator-utils lsscsi sg3_utils
 dnf install -y setroubleshoot-server "selinux-policy*" setools-console checkpolicy policycoreutils policycoreutils-python-utils policycoreutils-restorecond
@@ -224,7 +224,7 @@ dnf repository-packages epel-playground list > /tmp/command-log_dnf_repository-p
 dnf --enablerepo=epel install -y atop collectd collectd-utils iftop inotify-tools zstd
 
 # Package Install RHEL System Administration Tools (from EPEL-Playground Repository)
-# dnf --enablerepo=epel-playground install -y moreutils moreutils-parallel
+# dnf --enablerepo=epel-playground install -y glances htop jnettop moreutils moreutils-parallel ncdu srm
 
 #-------------------------------------------------------------------------------
 # Set AWS Instance MetaData
