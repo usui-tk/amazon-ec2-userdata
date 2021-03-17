a7d46fe109c907a7154b394022824bf5049c53a5
Takayuki Usui <quantum777@gmail.com>
2021-03-18 00:22:33 +0900

    Update 3rd-Bootstrap_RHEL-v6-HVM.sh

--- 3rd-Bootstrap_RHEL-v6-HVM.sh
+++ 3rd-Bootstrap_RHEL-v6-HVM.sh
@@ -120,7 +120,7 @@ if [ ${ELS_FLAG} -ne 0 ]; then
 	ELS_REPO_RPM="https://raw.githubusercontent.com/usui-tk/amazon-ec2-userdata/master/Workaround/Package/rh-amazon-rhui-client-els-3.0.39-1.el6.noarch.rpm"
 
 	# Remove RHUI configuration RPMs that do not support RHEL ELS
-	if [ $(rpm -qa | grep -ie "rh-amazon-rhui-client") ]; then
+	if [ $(rpm -qi "rh-amazon-rhui-client") ]; then
 		rpm -qi rh-amazon-rhui-client
 
 		yum remove -y rh-amazon-rhui-client
