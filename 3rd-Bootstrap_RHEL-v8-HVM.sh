c16dda12488c4ba5e4991371eb4954a33f05171e
Takayuki Usui <quantum777@gmail.com>
2021-04-05 22:44:07 +0900

    Add Package - yamllint from EPEL

--- 3rd-Bootstrap_RHEL-v8-HVM.sh
+++ 3rd-Bootstrap_RHEL-v8-HVM.sh
@@ -309,7 +309,7 @@ dnf repository-packages epel list > /tmp/command-log_dnf_repository-package-list
 dnf repository-packages epel-playground list > /tmp/command-log_dnf_repository-package-list_epel-playground.txt
 
 # Package Install RHEL System Administration Tools (from EPEL Repository)
-dnf --enablerepo=epel install -y atop bcftools bpytop byobu collectd collectd-utils colordiff dateutils fping glances htop httping iftop inotify-tools ipv6calc moreutils moreutils-parallel ncdu nload screen srm tcping zstd
+dnf --enablerepo=epel install -y atop bcftools bpytop byobu collectd collectd-utils colordiff dateutils fping glances htop httping iftop inotify-tools ipv6calc moreutils moreutils-parallel ncdu nload screen srm tcping yamllint zstd
 
 # Package Install RHEL System Administration Tools (from EPEL-Playground Repository)
 # dnf --enablerepo=epel-playground install -y jnettop wdiff
