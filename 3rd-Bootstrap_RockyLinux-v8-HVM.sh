b7f6886421ba6f7ee61fa4af9561fcea6a17c74b
Takayuki Usui <quantum777@gmail.com>
2021-07-22 01:53:15 +0900

    Update 3rd-Bootstrap_RockyLinux-v8-HVM.sh

--- 3rd-Bootstrap_RockyLinux-v8-HVM.sh
+++ 3rd-Bootstrap_RockyLinux-v8-HVM.sh
@@ -261,7 +261,8 @@ dnf repository-packages epel list > /tmp/command-log_dnf_repository-package-list
 dnf repository-packages epel-playground list > /tmp/command-log_dnf_repository-package-list_epel-playground.txt
 
 # Package Install Rocky Linux System Administration Tools (from EPEL Repository)
-dnf --enablerepo="epel" install -y atop bcftools bpytop byobu collectd collectd-utils colordiff dateutils fping glances htop httping iftop inotify-tools ipv6calc moreutils moreutils-parallel ncdu nload screen srm tcping yamllint zstd
+dnf --enablerepo="epel" install -y atop bcftools bpytop byobu collectd collectd-utils colordiff dateutils fping glances htop httping iftop inotify-tools ipv6calc ncdu nload screen srm tcping yamllint zstd
+# dnf --enablerepo="epel" install -y atop bcftools bpytop byobu collectd collectd-utils colordiff dateutils fping glances htop httping iftop inotify-tools ipv6calc moreutils moreutils-parallel ncdu nload screen srm tcping yamllint zstd
 
 # Package Install Rocky Linux System Administration Tools (from EPEL-Playground Repository)
 # dnf --enablerepo="epel-playground" install -y jnettop wdiff
