ea7fcf978c9f900cd211ca98e730b35ec66f16e5
Takayuki Usui <quantum777@gmail.com>
2022-05-02 04:06:24 +0900

    Update 3rd-Bootstrap_Ubuntu-20.04-LTS-HVM.sh

--- 3rd-Bootstrap_Ubuntu-20.04-LTS-HVM.sh
+++ 3rd-Bootstrap_Ubuntu-20.04-LTS-HVM.sh
@@ -47,7 +47,8 @@ CWAgentConfig="https://raw.githubusercontent.com/usui-tk/amazon-ec2-userdata/mas
 #	 https://cloud-images.ubuntu.com/locator/ec2/
 #    https://help.ubuntu.com/community/EC2StartersGuide
 #
-#    https://aws.amazon.com/marketplace/pp/
+#    https://aws.amazon.com/marketplace/pp/prodview-iftkyuwv2sjxi
+#    https://aws.amazon.com/marketplace/pp/prodview-zvdilnwnuopoo
 #
 #-------------------------------------------------------------------------------
 
@@ -276,7 +277,7 @@ fi
 # Get the latest AMI information of the OS type of this EC2 instance from Public AMI
 if [ -n "$RoleName" ]; then
 	echo "# Get Newest AMI Information from Public AMI"
-	NewestAmiInfo=$(aws ec2 describe-images --owner "099720109477" --filter "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*" "Name=virtualization-type,Values=hvm" "Name=architecture,Values=x86_64" --query 'sort_by(Images[].{YMD:CreationDate,Name:Name,ImageId:ImageId},&YMD)|reverse(@)|[0]' --output json --region ${Region})
+	NewestAmiInfo=$(aws ec2 describe-images --owner "099720109477" --filter "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-focal-20.04-arm64-server-*" "Name=virtualization-type,Values=hvm" "Name=architecture,Values=x86_64" --query 'sort_by(Images[].{YMD:CreationDate,Name:Name,ImageId:ImageId},&YMD)|reverse(@)|[0]' --output json --region ${Region})
 	NewestAmiId=$(echo $NewestAmiInfo| jq -r '.ImageId')
 	aws ec2 describe-images --image-ids ${NewestAmiId} --output json --region ${Region}
 fi
