6d0ef994c7d73233508b9c2d126dc1ae6d75d8dc
Takayuki Usui <quantum777@gmail.com>
2022-08-07 02:16:05 +0900

    Update

--- 3rd-Bootstrap_RHEL-v9-HVM.sh
+++ 3rd-Bootstrap_RHEL-v9-HVM.sh
@@ -380,8 +380,8 @@ dnf repository-packages epel list > /tmp/command-log_dnf_repository-package-list
 dnf repository-packages epel-testing list > /tmp/command-log_dnf_repository-package-list_epel-testing.txt
 
 # Package Install RHEL System Administration Tools (from EPEL Repository)
-dnf --enablerepo="epel" install -y atop colordiff dateutils fping htop iftop inotify-tools inxi ipv6calc moreutils moreutils-parallel ncdu screen stressapptest wdiff
-# dnf --enablerepo="epel" install -y atop bcftools bpytop byobu collectd collectd-utils colordiff dateutils fping glances htop httping iftop inotify-tools inxi ipv6calc jnettop moreutils moreutils-parallel ncdu nload screen srm stressapptest tcping wdiff yamllint
+dnf --enablerepo="epel" install -y atop colordiff dateutils fping htop iftop inotify-tools inxi ipv6calc jc moreutils moreutils-parallel ncdu screen stressapptest wdiff
+# dnf --enablerepo="epel" install -y atop bcftools bpytop byobu collectd collectd-utils colordiff dateutils fping glances htop httping iftop inotify-tools inxi ipv6calc jc jnettop moreutils moreutils-parallel ncdu nload screen srm stressapptest tcping wdiff yamllint
 
 # Package Install EC2 instance optimization tools (from EPEL Repository)
 # dnf --enablerepo="epel" install -y ec2-hibinit-agent
