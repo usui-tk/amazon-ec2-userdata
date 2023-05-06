4b2e6caacc7993d5c259342ff8f51aafb342b2e1
Takayuki Usui <quantum777@gmail.com>
2023-05-06 19:00:43 +0900

    Update 2nd-Decision_WindowsServer-Version.ps1

--- 2nd-Decision_WindowsServer-Version.ps1
+++ 2nd-Decision_WindowsServer-Version.ps1
@@ -27,6 +27,14 @@
 #               [Windows_Server-2016-Japanese-Full-Base-YYYY.MM.DD]
 #               [Windows_Server-2016-English-Full-Base-YYYY.MM.DD]
 #
+#      - 10.0 : Windows Server 2019 (Microsoft Windows Server 2019 [Datacenter Edition])
+#               [Windows_Server-2019-Japanese-Full-Base-YYYY.MM.DD]
+#               [Windows_Server-2019-English-Full-Base-YYYY.MM.DD]
+#
+#      - 10.0 : Windows Server 2022 (Microsoft Windows Server 2022 [Datacenter Edition])
+#               [Windows_Server-2022-Japanese-Full-Base-YYYY.MM.DD]
+#               [Windows_Server-2022-English-Full-Base-YYYY.MM.DD]
+#
 ########################################################################################################################
 
 
@@ -42,8 +50,8 @@ Set-Variable -Name USERDATA_LOG -Scope Script "$TEMP_DIR\userdata.log"
 Set-Variable -Name TRANSCRIPT_LOG -Scope Script "$TEMP_DIR\userdata-transcript-2nd.log"
 
 # Set Script Parameter for 3rd-Bootstrap Script (User Defined)
-
-Set-Variable -Name BOOTSTRAP_URL -Scope Script -Value "https://raw.githubusercontent.com/usui-tk/amazon-ec2-userdata/master/3rd-Bootstrap_WindowsServer.ps1"
+Set-Variable -Name BOOTSTRAP_URL_MODERN -Scope Script -Value "https://raw.githubusercontent.com/usui-tk/amazon-ec2-userdata/master/3rd-Bootstrap_WindowsServer.ps1"
+Set-Variable -Name BOOTSTRAP_URL_LEGACY -Scope Script -Value "https://raw.githubusercontent.com/usui-tk/amazon-ec2-userdata/master/3rd-Bootstrap_WindowsServer.ps1"
 Set-Variable -Name BOOTSTRAP_SCRIPT -Scope Script -Value "3rd-Bootstrap_WindowsServer.ps1"
 
 
@@ -107,6 +115,7 @@ Write-LogSeparator "Windows Server OS Decision"
 #Get OS Infomation & Language
 $Local:OSLanguage = ([CultureInfo]::CurrentCulture).IetfLanguageTag
 $Local:OSversion = (Get-CimInstance Win32_OperatingSystem | Select-Object Version).Version
+$Local:OSBuildNumber = (Get-CimInstance Win32_OperatingSystem | Select-Object BuildNumber).BuildNumber
 
 Write-Log ("# [Windows Infomation] OS Version : " + ($OSversion) + " - OS Language : " + ($OSLanguage))
 
@@ -114,42 +123,62 @@ Write-Log ("# [Windows Infomation] OS Version : " + ($OSversion) + " - OS Langua
 if ($OSversion -match "^6.1.*") {
     # Log Separator
     Write-LogSeparator "# [Bootstrap Script] : Microsoft Windows Server 2008 R2"
-
-    Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL))
-    Invoke-WebRequest -Uri $BOOTSTRAP_URL -OutFile $BOOTSTRAP_SCRIPT
+    Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL_LEGACY))
+    Invoke-WebRequest -Uri $BOOTSTRAP_URL_LEGACY -OutFile $BOOTSTRAP_SCRIPT
     Write-Log "# Script Execution 2nd-Decision Script [COMPLETE] : $ScriptFullPath"
-    powershell.exe -ExecutionPolicy Bypass .\$BOOTSTRAP_SCRIPT -SkipNetworkProfileCheck
-
+    powershell.exe -ExecutionPolicy Bypass "$TEMP_DIR\$BOOTSTRAP_SCRIPT" -SkipNetworkProfileCheck
 }
 elseif ($OSversion -match "^6.2.*") {
     # Log Separator
     Write-LogSeparator "# [Bootstrap Script] : Microsoft Windows Server 2012"
-
-    Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL))
-    Invoke-WebRequest -Uri $BOOTSTRAP_URL -OutFile $BOOTSTRAP_SCRIPT
+    Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL_LEGACY))
+    Invoke-WebRequest -Uri $BOOTSTRAP_URL_LEGACY -OutFile $BOOTSTRAP_SCRIPT
     Write-Log "# Script Execution 2nd-Decision Script [COMPLETE] : $ScriptFullPath"
-    powershell.exe -ExecutionPolicy Bypass .\$BOOTSTRAP_SCRIPT -SkipNetworkProfileCheck
-
+    powershell.exe -ExecutionPolicy Bypass "$TEMP_DIR\$BOOTSTRAP_SCRIPT" -SkipNetworkProfileCheck
 }
 elseif ($OSversion -match "^6.3.*") {
     # Log Separator
     Write-LogSeparator "# [Bootstrap Script] : Microsoft Windows Server 2012 R2"
-
-    Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL))
-    Invoke-WebRequest -Uri $BOOTSTRAP_URL -OutFile $BOOTSTRAP_SCRIPT
+    Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL_LEGACY))
+    Invoke-WebRequest -Uri $BOOTSTRAP_URL_LEGACY -OutFile $BOOTSTRAP_SCRIPT
     Write-Log "# Script Execution 2nd-Decision Script [COMPLETE] : $ScriptFullPath"
-    powershell.exe -ExecutionPolicy Bypass .\$BOOTSTRAP_SCRIPT -SkipNetworkProfileCheck
-
+    powershell.exe -ExecutionPolicy Bypass "$TEMP_DIR\$BOOTSTRAP_SCRIPT" -SkipNetworkProfileCheck
 }
 elseif ($OSversion -match "^10.0.*") {
-    # Log Separator
-    Write-LogSeparator "# [Bootstrap Script] : Microsoft Windows Server 2016"
-
-    Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL))
-    Invoke-WebRequest -Uri $BOOTSTRAP_URL -OutFile $BOOTSTRAP_SCRIPT
-    Write-Log "# Script Execution 2nd-Decision Script [COMPLETE] : $ScriptFullPath"
-    powershell.exe -ExecutionPolicy Bypass .\$BOOTSTRAP_SCRIPT -SkipNetworkProfileCheck
-
+    switch ($OSBuildNumber) {
+        '14393' {
+            # Log Separator
+            Write-LogSeparator "# [Bootstrap Script] : Microsoft Windows Server 2016"
+            Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL_MODERN))
+            Invoke-WebRequest -Uri $BOOTSTRAP_URL_MODERN -OutFile $BOOTSTRAP_SCRIPT
+            Write-Log "# Script Execution 2nd-Decision Script [COMPLETE] : $ScriptFullPath"
+            powershell.exe -ExecutionPolicy Bypass "$TEMP_DIR\$BOOTSTRAP_SCRIPT" -SkipNetworkProfileCheck
+        }
+        '17763' {
+            # Log Separator
+            Write-LogSeparator "# [Bootstrap Script] : Microsoft Windows Server 2019"
+            Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL))
+            Invoke-WebRequest -Uri $BOOTSTRAP_URL -OutFile $BOOTSTRAP_SCRIPT
+            Write-Log "# Script Execution 2nd-Decision Script [COMPLETE] : $ScriptFullPath"
+            powershell.exe -ExecutionPolicy Bypass "$TEMP_DIR\$BOOTSTRAP_SCRIPT" -SkipNetworkProfileCheck
+        }
+        '20348' {
+            # Log Separator
+            Write-LogSeparator "# [Bootstrap Script] : Microsoft Windows Server 2022"
+            Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL))
+            Invoke-WebRequest -Uri $BOOTSTRAP_URL -OutFile $BOOTSTRAP_SCRIPT
+            Write-Log "# Script Execution 2nd-Decision Script [COMPLETE] : $ScriptFullPath"
+            powershell.exe -ExecutionPolicy Bypass "$TEMP_DIR\$BOOTSTRAP_SCRIPT" -SkipNetworkProfileCheck
+        }
+        default {
+            # Log Separator
+            Write-LogSeparator "# [Bootstrap Script] : Microsoft Windows Server (UnKnown Modern OS)"
+            Write-Log ("# [Bootstrap Script] : " + ($BOOTSTRAP_URL))
+            Invoke-WebRequest -Uri $BOOTSTRAP_URL -OutFile $BOOTSTRAP_SCRIPT
+            Write-Log "# Script Execution 2nd-Decision Script [COMPLETE] : $ScriptFullPath"
+            powershell.exe -ExecutionPolicy Bypass "$TEMP_DIR\$BOOTSTRAP_SCRIPT" -SkipNetworkProfileCheck
+        }
+    }
 }
 else {
     Write-Log ("# [Warning] No Target - Windows NT Version Information : " + $OSversion)
