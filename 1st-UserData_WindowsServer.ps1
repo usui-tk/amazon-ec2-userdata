854794662348954927d89ffa297238ae41abb9ec
Takayuki Usui <quantum777@gmail.com>
2023-05-07 18:15:03 +0900

    Update 1st-UserData_WindowsServer.ps1

--- 1st-UserData_WindowsServer.ps1
+++ 1st-UserData_WindowsServer.ps1
@@ -6,7 +6,7 @@ Set-Variable -Name DecisionScript -Option Constant -Scope Script -Value "https:/
 
 Set-Location -Path "$Env:SystemRoot\Temp"
 
-Invoke-WebRequest -Uri $DecisionScript -OutFile 2nd-Decision_WindowsServer-Version.ps1
+Invoke-WebRequest -Uri $DecisionScript -UseBasicParsing -OutFile 2nd-Decision_WindowsServer-Version.ps1
 
 powershell.exe -ExecutionPolicy Bypass .\2nd-Decision_WindowsServer-Version.ps1 -SkipNetworkProfileCheck
 </powershell>
