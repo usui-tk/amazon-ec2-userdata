9fc0e4f8a2699f02e3a14bcec6305b3f23f331bc
Takayuki Usui <quantum777@gmail.com>
2024-02-24 01:03:06 +0900

    Update EL8

--- 3rd-Bootstrap_CentOS-v8-HVM.sh
+++ 3rd-Bootstrap_CentOS-v8-HVM.sh
@@ -308,7 +308,7 @@ dnf repository-packages epel-testing list > /tmp/command-log_dnf_repository-pack
 dnf repository-packages epel-testing-modular list > /tmp/command-log_dnf_repository-package-list_epel-testing-modular.txt
 
 # Package Install CentOS System Administration Tools (from EPEL Repository)
-dnf --enablerepo="epel" install -y atop bcftools bpytop byobu collectd collectd-utils colordiff dateutils fping glances htop httping iftop inotify-tools inxi ipv6calc moreutils moreutils-parallel ncdu nload screen srm stressapptest tcping unicornscan wdiff yamllint
+dnf --enablerepo="epel" install -y atop bash-color-prompt bcftools bpytop byobu collectd collectd-utils colordiff dateutils fping glances htop httping iftop inotify-tools inxi ipv6calc moreutils moreutils-parallel ncdu nload screen srm stressapptest tcping unicornscan wdiff yamllint
 
 # Package Install EC2 instance optimization tools (from EPEL Repository)
 dnf --enablerepo="epel" install -y ec2-hibinit-agent
