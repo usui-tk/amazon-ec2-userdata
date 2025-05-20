59cb41ec2a7d206a1f81fb0413d6954dac33cd0f
Takayuki Usui <quantum777@gmail.com>
2025-05-21 01:26:10 +0900

    Update 3rd-Bootstrap_SLES-v12-HVM.sh

--- 3rd-Bootstrap_SLES-v12-HVM.sh
+++ 3rd-Bootstrap_SLES-v12-HVM.sh
@@ -853,9 +853,11 @@ if [ -n "$RoleName" ]; then
 	# Get the latest AMI information of the OS type of this EC2 instance from Public AMI
 	echo "# Get Amazon Machine Image Information"
 
-	NewestAmiId=$(aws ec2 describe-images --owner 013907871322 --filter "Name=name,Values=suse-sles-12-*-hvm-ssd-x86_64" "Name=virtualization-type,Values=hvm" "Name=architecture,Values=x86_64" --query 'sort_by(Images[].{YMD:CreationDate,Name:Name,ImageId:ImageId},&YMD)' --output text --region ${Region} | grep -v byos | grep -v ecs | grep -v sapcal | sort -k 3 --reverse | head -n 1 | awk '{print $1}')
-	NewestAmiId=$(echo $NewestAmiInfo| jq -r '.ImageId')
-	aws ec2 describe-images --image-ids ${NewestAmiId} --output json --region ${Region} > "/var/log/user-data_aws-cli_amazon-machine-images_describe-describe-images.txt"
+	LatestAmiId=$(aws ec2 describe-images --owner "013907871322" --filter "Name=name,Values=suse-sles-12-*" "Name=virtualization-type,Values=hvm" "Name=architecture,Values=x86_64" --query 'sort_by(Images[].{YMD:CreationDate,Name:Name,ImageId:ImageId},&YMD)|reverse(@)' --output json --region ${Region} | jq -r '[.[] | select(contains({Name: "BETA"}) | not)] | .[0].ImageId')
+
+	if [ -n "$LatestAmiId" ]; then
+		aws ec2 describe-images --image-ids ${LatestAmiId} --output json --region ${Region} > "/var/log/user-data_aws-cli_amazon-machine-images_describe-describe-images.txt"
+	fi
 fi
 
 #-------------------------------------------------------------------------------
