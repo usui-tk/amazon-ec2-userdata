85fdf5655061ca0b5ebec542a5d737acc455fa40
Takayuki Usui <quantum777@gmail.com>
2025-06-29 22:50:52 +0900

    Update 3rd-Bootstrap_OracleLinux-v9-HVM.sh

--- 3rd-Bootstrap_OracleLinux-v9-HVM.sh
+++ 3rd-Bootstrap_OracleLinux-v9-HVM.sh
@@ -180,7 +180,7 @@ done
 dnf update -y
 
 # Switching Linux-kernel packages (Switch from RHEL compatible kernel to Unbreakable Enterprise Kernel)
-# https://docs.oracle.com/en/operating-systems/uek/7/relnotes7.0/index.html
+# https://docs.oracle.com/en/operating-systems/uek/index.html
 
 if [ $(grubby --default-kernel | grep -ie "el9uek") ]; then
 	echo "Linux Kernel Package Name : kernel-uek"
